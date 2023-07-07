ad53e27510a60373a8697325165278d5f04c8ee0
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-07-07 18:02:15 +0200

    add test

---
+++ models/test/employee.test.js
@@ -0,0 +1,69 @@
+const Employee = require('../employees.model');
+const expect = require('chai').expect;
+
+describe('Employee', () => {
+
+    it('should throw an error if no "firstName" arg', () => {
+        const dep = new Employee({});
+        dep.validate(err => {
+            expect(err.errors.firstName).to.exist;
+        });
+    });
+
+    it('should throw an error if no "lastName" arg', () => {
+        const dep = new Employee({});
+        dep.validate(err => {
+            expect(err.errors.lastName).to.exist;
+        });
+    });
+
+    it('should throw an error if no "department" arg', () => {
+        const dep = new Employee({});
+        dep.validate(err => {
+            expect(err.errors.department).to.exist;
+        });
+    });
+
+    it('should throw an error if "firstName" is not a string', () => {
+        const cases = [{}, []];
+        for (let firstName of cases) {
+            const dep = new Employee({ firstName });
+            dep.validate(err => {
+                expect(err.errors.firstName).to.exist;
+            });
+        }
+    });
+
+    it('should throw an error if "lastName" is not a string', () => {
+        const cases = [{}, []];
+        for (let lastName of cases) {
+            const dep = new Employee({ lastName });
+            dep.validate(err => {
+                expect(err.errors.lastName).to.exist;
+            });
+        }
+    });
+
+    it('should throw an error if "department" is not a string', () => {
+        const cases = [{}, []];
+        for (let department of cases) {
+            const dep = new Employee({ department });
+            dep.validate(err => {
+                expect(err.errors.department).to.exist;
+            });
+        }
+    });
+
+    it('should not throw an error if "firstName", "lastName" and "department" is okay', () => {
+        const cases = [
+            { firstName: 'John', lastName: 'Doe', department: 'IT' },
+            { firstName: 'Ann', lastName: 'Doe', department: 'Marketing' }
+        ];
+        for (let name of cases) {
+            const dep = new Employee(name);
+            dep.validate(err => {
+                expect(err).to.not.exist;
+            });
+        }
+    });
+});
